6c36370438b55a2365a3ff7b0cd988c4e82db50e
ditclear <ditclear@qq.com>
2019-06-02 13:06:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+
+
+# flutter_web_challenge_googlemaps_web
+
+Use Flutter_web💪  to implement the UI challenge of Google Maps on [uplabs](https://www.uplabs.com/posts/google-maps-redesign-919dd0d6-0883-4378-b5b2-f77e36adb6b5) .
+
+[中文版本](README_CN.md)
+
+> Designer : [Joydeep Sengupta](https://www.uplabs.com/joydeeproni)
+>
+> Uplabs : <https://www.uplabs.com/posts/google-maps-redesign-919dd0d6-0883-4378-b5b2-f77e36adb6b5>
+>
+> Developer : [ditclear](https://github.com/ditclear)
+
+#### Run
+
+First, make sure the relevant environment is installed, please refer to [Flutter-web] (https://github.com/flutter/flutter_web).
+
+Then clone
+
+> git clone https://github.com/flutter-ui-challenges/flutter_web_challenge_googlemaps.git
+
+You can open the project with Android Studio, IntelliJ or Visual Studio Code and run the `main.dart` file.
+
+### Challenge 
+
+![](ui/all.png)
+
+
+
+![](ui/attachment.gif)
+
+
+
+![](ui/preview.gif)
+
+
+
+#### 不用客气，buy Me a Lunch ~
+
+|                            Alipay                            |                          WeChatPay                           |
+| :----------------------------------------------------------: | :----------------------------------------------------------: |
+| ![](https://camo.githubusercontent.com/fbaa3ecc28af039c8e6011ddf473e51d2a08c8d2/68747470733a2f2f75706c6f61642d696d616765732e6a69616e7368752e696f2f75706c6f61645f696d616765732f333732323639352d666664653137306665633933316561652e706e673f696d6167654d6f6772322f6175746f2d6f7269656e742f7374726970253743696d61676556696577322f322f772f333430) | ![](https://camo.githubusercontent.com/ade43488617a69c72d5be1841fd4d0934689a3b0/68747470733a2f2f75706c6f61642d696d616765732e6a69616e7368752e696f2f75706c6f61645f696d616765732f333732323639352d316238323761353465633237346535392e706e673f696d6167654d6f6772322f6175746f2d6f7269656e742f7374726970253743696d61676556696577322f322f772f333430) |
+
+### License
+
+```
+Copyright 2019 ditclear
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/components/explore_content_widget.dart
@@ -0,0 +1,162 @@
+import 'package:flutter_web/material.dart';
+import 'package:flutter_web_challenge_googlemaps/helper/ui_helper.dart';
+
+class ExploreContentWidget extends StatelessWidget {
+  final double currentExplorePercent;
+  final placeName = const ["Authentic\nrestaurant", "Famous\nmonuments", "Weekend\ngetaways"];
+  const ExploreContentWidget({Key key, this.currentExplorePercent}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (currentExplorePercent != 0) {
+      return Positioned(
+        top: realH(standardHeight + (162 - standardHeight) * currentExplorePercent),
+        width: screenWidth,
+        child: Container(
+          height: screenHeight,
+          child: ListView(
+            physics: const BouncingScrollPhysics(),
+            shrinkWrap: true,
+            children: <Widget>[
+              Opacity(
+                opacity: currentExplorePercent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                      child: Transform.translate(
+                        offset: Offset(screenWidth / 3 * (1 - currentExplorePercent),
+                            screenWidth / 3 / 2 * (1 - currentExplorePercent)),
+                        child: Image.asset(
+                          "icon_1.png",
+                          width: realH(133),
+                          height: realH(133),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Image.asset(
+                        "icon_2.png",
+                        width: realH(133),
+                        height: realH(133),
+                      ),
+                    ),
+                    Expanded(
+                      child: Transform.translate(
+                        offset: Offset(-screenWidth / 3 * (1 - currentExplorePercent),
+                            screenWidth / 3 / 2 * (1 - currentExplorePercent)),
+                        child: Image.asset(
+                          "icon_3.png",
+                          width: realH(133),
+                          height: realH(133),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Transform.translate(
+                  offset: Offset(0, realH(23 + 380 * (1 - currentExplorePercent))),
+                  child: Opacity(
+                      opacity: currentExplorePercent,
+                      child: Container(
+                        width: screenWidth,
+                        height: realH(172 + (172 * 4 * (1 - currentExplorePercent))),
+                        child: ListView(
+                          physics: const BouncingScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(left: realW(22)),
+                            ),
+                            buildListItem(0, "Authentic\nrestaurant"),
+                            buildListItem(1, "Famous\nmonuments"),
+                            buildListItem(2, "Weekend\ngetaways"),
+                            buildListItem(3, "Authentic\nrestaurant"),
+                            buildListItem(4, "Famous\nmonuments"),
+                            buildListItem(5, "Weekend\ngetaways"),
+                          ],
+                        ),
+                      ))),
+              Transform.translate(
+                  offset: Offset(0, realH(58 + 570 * (1 - currentExplorePercent))),
+                  child: Opacity(
+                    opacity: currentExplorePercent,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: realW(22)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(left: realW(22)),
+                            child: Text("EVENTS",
+                                style:
+                                    const TextStyle(color: Colors.white54, fontSize: 13, fontWeight: FontWeight.bold)),
+                          ),
+                          Stack(
+                            children: <Widget>[
+                              Image.asset(
+                                "dj.png",
+                              ),
+                              Positioned(
+                                  bottom: realH(26),
+                                  left: realW(24),
+                                  child: Text(
+                                    "Marshmello Live in Concert",
+                                    style: TextStyle(color: Colors.white, fontSize: realW(16)),
+                                  ))
+                            ],
+                          ),
+                          Transform.translate(
+                            offset: Offset(0, realH(30 - 30 * (currentExplorePercent - 0.75) * 4)),
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(
+                                  child: Image.asset("banner_4.png"),
+                                ),
+                                Expanded(
+                                  child: Image.asset("banner_5.png"),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+              Padding(
+                padding: EdgeInsets.only(bottom: realH(262)),
+              )
+            ],
+          ),
+        ),
+      );
+    } else {
+      return const Padding(
+        padding: const EdgeInsets.all(0),
+      );
+    }
+  }
+
+  buildListItem(int index, String name) {
+    return Transform.translate(
+      offset: Offset(0, index * realH(127) * (1 - currentExplorePercent)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image.asset(
+            "banner_${index % 3 + 1}.png",
+            width: realH(127),
+            height: realH(127),
+          ),
+          Text(
+            placeName[index % 3],
+            style: TextStyle(color: Colors.white, fontSize: realH(16)),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
